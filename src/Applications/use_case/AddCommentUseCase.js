7c8e9042d86d99c8e9c96f0bc0915d20b810fb39
Ilham J.E <ilham5566@gmail.com>
2023-12-09 15:58:44 +0800

    fix bug : solve test

--- src/Applications/use_case/AddCommentUseCase.js
+++ src/Applications/use_case/AddCommentUseCase.js
@@ -1,4 +1,4 @@
-const NewComment = require('../../Domains/comments/entities/NewComment');
+const NewComment = require("../../Domains/comments/entities/NewComment");
 
 class AddCommentUseCase {
   constructor({ commentRepository, threadRepository }) {
@@ -7,28 +7,16 @@ class AddCommentUseCase {
   }
 
   async execute(useCasePayload) {
-    /**
-     * @TODO 7
-     * Tuliskan kode alur logika dalam menambahkan komentar baru yang diambil dari `useCasePayload`
-     * ke dalam database.
-     *
-     * Catatan:
-     * - Manfaatkanlah entities `NewComment` untuk memastikan nilai `useCasePayload` valid.
-     * - Gunakan `this._threadRepository` dan method abstract di dalamnya
-     *   untuk berinteraksi dengan database.
-     * - Jika thread tidak ditemukan (dilihat dari `threadId`),
-     *   bangkitkan error dengan pesan 'ADD_COMMENT_USE_CASE.THREAD_NOT_FOUND'
-     * - Kembalikan method `execute` dengan nilai yang dihasilkan dari pemanggilan
-     *   fungsi `this._commentRepository.addComment(newComment);`
-     */
-
     const newComment = new NewComment(useCasePayload);
+
+    // Memastikan nilai payload yang diterima adalah valid
     const isThreadExist = await this._threadRepository.isThreadExist(newComment.threadId);
 
     if (!isThreadExist) {
-      throw new Error('ADD_COMMENT_USE_CASE.THREAD_NOT_FOUND');
+      throw new Error("ADD_COMMENT_USE_CASE.THREAD_NOT_FOUND");
     }
 
+    // Menambahkan komentar baru ke dalam database menggunakan commentRepository
     return this._commentRepository.addComment(newComment);
   }
 }
